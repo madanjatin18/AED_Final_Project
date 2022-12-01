35b1bd846558447320eb00e77314762846b29dfb
Akhilesh Dongre <114017380+akh-04@users.noreply.github.com>
2022-11-30 23:04:14 -0500

    Worked on Enterprise and Employee pkg

---
+++ CommunityWelfareApplication/src/BusinessModel/Enterprise/SupplierEnterprise.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BusinessModel.Enterprise;
+import Business.Role.SupplierManagerRole;
+import Business.Role.Role;
+import java.util.HashSet;
+
+/**
+ *
+ * @author hp
+ */
+
+public class SupplierEnterprise extends Enterprise 
+{
+    public SupplierEnterprise(String name)
+    {
+        super(name, Enterprise.EnterpriseType.Supplier);
+        
+    } 
+    @Override
+    public HashSet<Role> getSupportedRole(){
+        new SupplierManagerRole();
+        return role;
+        
+    }
+}
+
+
+
+
+
+
