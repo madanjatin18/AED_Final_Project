22ffabfdda4c489221b881525636dc6b50858cb5
Guest User <Guest User@10.0.0.212>
2022-12-01 21:54:27 -0500

    Checking Netbeans connection to git

---
+++ CommunityWelfareApplication/src/BusinessModel/Organization/HelpProviderOrganization.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BusinessModel.Organization;
+import Business.HelpProvider.HelpProviderDirectory;
+import Business.Role.HelpProviderRole;
+import Business.Role.Role;
+import java.util.HashSet;
+
+/**
+ *
+ * @author hp
+ */
+public class HelpProviderOrganization extends Organization  
+{
+    
+    HelpProviderDirectory helpproviderlist;
+    
+    public HelpProviderOrganization()
+    {
+        super(Organization.Type.HelpProvider.getValue());
+        helpproviderlist = new HelpProviderDirectory();
+    }
+
+    public HelpProviderDirectory getHelpproviderlist() {
+        return helpproviderlist;
+    }
+
+    public void setHelpproviderlist(HelpProviderDirectory helpproviderlist) 
+    {
+        this.helpproviderlist = helpproviderlist;
+    }
+    
+    
+    @Override
+    public HashSet<Role> getSupportedRole(){
+       role.add(new HelpProviderRole());
+       return role;
+       
+    }
+    
+    
+}
+
+
+
+
+
+
+
+
---
+++ CommunityWelfareApplication/src/BusinessModel/Organization/HelpSeekerOrganization.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BusinessModel.Organization;
+import Business.HelpSeeker.HelpSeekerDirectory;
+import Business.Role.HelpSeekerRole;
+import Business.Role.Role;
+import java.util.HashSet;
+
+/**
+ *
+ * @author hp
+ */
+
+public class HelpSeekerOrganization extends Organization{
+   HelpSeekerDirectory helpseekerlist;
+    public HelpSeekerOrganization()
+    {
+        super(Organization.Type.HelpSeeker.getValue());
+        helpseekerlist=new HelpSeekerDirectory();
+    }
+
+    public HelpSeekerDirectory getHelpseekerlist() {
+        return helpseekerlist;
+    }
+
+    public void setHelpseekerlist(HelpSeekerDirectory helpseekerlist) {
+        this.helpseekerlist = helpseekerlist;
+    }
+    
+    @Override
+    public HashSet<Role> getSupportedRole(){
+        role.add(new HelpSeekerRole());
+        return role;
+        
+    }
+    
+}
+
+
+
+
+
